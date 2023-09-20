6bdd0c6e1dcd0909e8fd43ed2dc279c952f901aa
Iyan Wardiana <iyanwardiana@gmail.com>
2023-09-20 14:46:29 +0700

    feat: perubahan submit form

--- application/views/v_project/v_opname/opname_inb_form.php
+++ application/views/v_project/v_opname/opname_inb_form.php
@@ -357,6 +357,8 @@
             $subTitleH  = "Opname";
             $subTitleD  = "persetujuan";
             $alert5     = 'Silahkan pilih status persetujuan.';
+			$alert6	    = "Detail item tidak boleh kosong.";
+			$alertSubmit= "data sudah berhasil disimpan";
             $SPeriode   = "Periode Mulai";
             $alertAcc   = "Belum diset kode akun penggunaan.";
 
@@ -381,6 +383,8 @@
             $subTitleH  = "Add Opname";
             $subTitleD  = "approval";
             $alert5     = 'Please select approval status.';
+			$alert6	    = "item detail can not empty.";
+			$alertSubmit= "data has been successfully saved";
             $SPeriode   = "Start Periode";
             $alertAcc   = "Not set account material usage.";
 
@@ -757,7 +761,7 @@
                 </form>
                 <!-- End -->
 
-                <form class="form-horizontal" name="frm" id="frm" method="post" action="<?php echo $form_action; ?>" enctype="multipart/form-data" onSubmit="return validateInData()">
+                <form class="form-horizontal" name="frm" id="frm" method="post" action="<?php echo $form_action; ?>" enctype="multipart/form-data">
                     <input type="hidden" name="IS_LAST" id="IS_LAST" value="<?php echo $IS_LAST; ?>">
                     <input type="hidden" name="APP_LEVEL" id="APP_LEVEL" value="<?php echo $APP_LEVEL; ?>">
                     <input type="hidden" name="decFormat" id="decFormat" value="<?php echo $decFormat; ?>" />
@@ -1975,7 +1979,7 @@
                             <script type="text/javascript">
                                 function hideBtn()
                                 {
-                                    document.getElementById('btnSave').style.display = 'none';
+                                    // document.getElementById('btnSave').style.display = 'none'; // sudah di handler pada saat addEventListener => submit
                                 }
                             </script>
                         </div>
@@ -2712,7 +2716,69 @@
         countGTotal();
     }
 
-    function validateInData()
+    const form = document.getElementById('frm');
+	form.addEventListener('submit', function(event) {
+		event.preventDefault();
+
+        var totrow  = document.getElementById('totalrow').value;
+
+        OPNH_STAT   = document.getElementById("OPNH_STAT").value;
+        OPNH_NOTE2  = document.getElementById("OPNH_NOTE2").value;
+
+        if(OPNH_STAT == 9 || OPNH_STAT == 4 || OPNH_STAT == 5)
+        {
+            OPNH_MEMO       = document.getElementById('OPNH_MEMO').value;
+            // if(OPNH_MEMO == '')
+            // {
+            //     swal('<?php // echo $alert1; ?>',
+            //     {
+            //         icon:"warning",
+            //     })
+            //     .then(function()
+            //     {
+            //         swal.close();
+            //         document.getElementById('OPNH_NOTE2').focus();
+            //     });
+            //     return false;
+            // }
+        }
+        else if(OPNH_STAT == 0)
+        {
+            swal('<?php echo $alert5; ?>',
+            {
+            icon:"warning",
+            });
+            document.getElementById('OPNH_STAT').focus();
+            document.getElementById('btnSave').style.display = '';
+            return false;
+        }
+
+		if(totrow == 0)
+		{
+			swal('<?php echo $alert6; ?>',
+			{
+				icon:"warning",
+			});
+			return false;
+		}
+
+		form.submit();
+
+		if($(form).data('submitted')==true){
+			document.getElementById('btnSave').style.display 		= 'none';
+			document.getElementById('btnBack').style.display 		= 'none';
+			swal('<?php echo $alertSubmit;?>');
+			return false;
+		} else {
+			//swal('submitting');
+			document.getElementById('btnSave').style.display 		= 'none';
+			document.getElementById('btnBack').style.display 		= 'none';
+			$(form).data('submitted', true);
+			return true;
+		}
+	});
+
+    function validateInData_230920() // dipindahkan ke addEventListener => submit
     {
         var totrow  = document.getElementById('totalrow').value;
 
